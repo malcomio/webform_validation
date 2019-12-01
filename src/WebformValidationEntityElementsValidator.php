8840aa6e615d29755e5b8d7fb56eee925f0b7915
Malcolm Young <malcolm@rubydesign.co.uk>
2019-12-01 21:29:46 +0000

    work in progress on entity validations form

---
+++ src/WebformValidationEntityElementsValidator.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Drupal\webform_validation;
+
+use Drupal\webform\WebformEntityElementsValidator;
+
+class WebformValidationEntityElementsValidator extends WebformEntityElementsValidator {
+
+}
